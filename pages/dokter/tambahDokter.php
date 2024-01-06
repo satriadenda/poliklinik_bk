6fef6e58b5e5dbbb44e78981aa0e1c76969b678a
satriadenda <91672636+satriadenda@users.noreply.github.com>
2024-01-06 23:31:38 +0700

    Add files via upload

---
+++ pages/dokter/tambahDokter.php
@@ -0,0 +1,31 @@
+<?php
+include '../../config/koneksi.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Ambil nilai dari form
+    $nama = $_POST["nama"];
+    $alamat = $_POST["alamat"];
+    $no_hp = $_POST["no_hp"];
+    $poli = $_POST["poli"];
+    $password = md5($nama);
+
+    // Query untuk menambahkan data obat ke dalam tabel
+    $query = "INSERT INTO dokter (nama, password, alamat, no_hp, id_poli) VALUES ('$nama', '$password', '$alamat', '$no_hp', '$poli')";
+    
+
+   
+    if (mysqli_query($mysqli, $query)) {
+        echo '<script>';
+        echo 'alert("Data dokter berhasil ditambahkan!");';
+        echo 'window.location.href = "../../dokter.php";';
+        echo '</script>';
+        exit();
+    } else {
+        // Jika terjadi kesalahan, tampilkan pesan error
+        echo "Error: " . $query . "<br>" . mysqli_error($mysqli);
+    }
+}
+
+// Tutup koneksi
+mysqli_close($mysqli);
+?>
